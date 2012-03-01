cab9687715681e66926a0822d6957d46f00f4ae7
Sina Iman <sina.iman@gmail.com>
2012-03-01 01:38:40 -0500

    added ability for custom options for fields generated by mongoid_denormalize to be passed through using the denormalize keyword by simply adding to the options hash. options other than :type, :from, and :to will be be treated as custom options. added a test to ensure this works by specifying :require => true to a field in the Comment model. The custom option in this test is benign, but ideally will be picked up by some other library to do useful things, for example the :spacial => true option for mongoid_spacial can now be passed on denormalized fields.

--- spec/app/models/comment.rb
+++ spec/app/models/comment.rb
@@ -7,9 +7,8 @@ class Comment
   belongs_to :post, :inverse_of => :comments
   belongs_to :user
 
-  
   denormalize :location, :type => Array, :from => :user
   denormalize :name, :from => :user
   denormalize :email, :from => :user
-  denormalize :created_at, :type => Time, :from => :post
+  denormalize :created_at, :type => Time, :from => :post, :required => true
 end
